10ca867ddb2dc76a41b6ae829108f6c0afbffa5c
Antony Lee <antony@antonylee.co.uk>
2024-12-19 14:27:22 +0000

    example service

---
+++ cmd/http-service/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root."))
+	})
+
+	http.ListenAndServe(":3333", r)
+
+}
