c3f25998c444725cbef608472ad18a708171bc45
Antony Lee <antony@antonylee.co.uk>
2025-01-01 17:06:38 +0000

    now expects a storer

--- pkg/twitch/refreshing_auth_provider.go
+++ pkg/twitch/refreshing_auth_provider.go
@@ -1,12 +1,16 @@
 package twitch
 
-type RefreshingAuthProvider struct{}
+type RefreshingAuthProvider struct {
+	store RefreshingAuthProviderStorer
+}
 
 func (p *RefreshingAuthProvider) GetAccessToken() (string, error) {
 	return "", nil
 }
 
-func NewRefreshingAuthProvider() (*RefreshingAuthProvider, error) {
-	authProvider := &RefreshingAuthProvider{}
+func NewRefreshingAuthProvider(store RefreshingAuthProviderStorer) (*RefreshingAuthProvider, error) {
+	authProvider := &RefreshingAuthProvider{
+		store,
+	}
 	return authProvider, nil
 }
