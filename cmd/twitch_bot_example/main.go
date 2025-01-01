8a467d59e79f80863b28edbfa4e5096ee335a37d
Antony Lee <antony@antonylee.co.uk>
2025-01-01 17:14:43 +0000

    create a store

--- cmd/twitch_bot_example/main.go
+++ cmd/twitch_bot_example/main.go
@@ -13,6 +13,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	// Create store
+	_, err = twitch.NewRefreshingAuthProviderFilesystemStore()
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Create bot
 	bot, err := twitch.NewBot()
 	if err != nil {
