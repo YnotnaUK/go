d29c8f01e154d438ec0ff678db4ac9a970f35b5b
Antony Lee <antony@antonylee.co.uk>
2025-01-01 16:46:54 +0000

    restarting module

--- pkg/twitch/bot.go
+++ pkg/twitch/bot.go
@@ -1,26 +1,8 @@
 package twitch
 
-type Bot struct {
-	auth AuthProvider
-}
+type TwitchBot struct{}
 
-func (b *Bot) Start() {
-
-}
-
-func NewBot() (*Bot, error) {
-	bot := &Bot{}
-	return bot, nil
-}
-
-// Creates a fully ready to use bot
-func NewSimpleBot(clientId string) (*Bot, error) {
-	authProvider, err := NewRefreshingAuthProvider()
-	if err != nil {
-		return nil, err
-	}
-	bot := &Bot{
-		auth: authProvider,
-	}
+func NewSimpleBot() (*TwitchBot, error) {
+	bot := &TwitchBot{}
 	return bot, nil
 }
