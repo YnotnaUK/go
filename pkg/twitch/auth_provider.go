df2b3a515a5bbfe171d86b7a39b6ab2631f07b57
Antony Lee <antony@antonylee.co.uk>
2025-01-02 11:27:45 +0000

    get auth provider functions working

--- pkg/twitch/auth_provider.go
+++ pkg/twitch/auth_provider.go
@@ -2,4 +2,6 @@ package twitch
 
 type AuthProvider interface {
 	GetAccessTokenByUserId(userId string) (string, error)
+	AddAccessToken(accessToken *AccessToken) error
+	AddAccessTokenFromFile(accessToken *AccessToken) error
 }
--- pkg/twitch/refreshing_auth_provider.go
+++ pkg/twitch/refreshing_auth_provider.go
@@ -1,7 +1,11 @@
 package twitch
 
+import (
+	"fmt"
+)
+
 type RefreshingAuthProvider struct {
-	accessTokens map[string]AccessToken
+	accessTokens map[string]*AccessToken
 	clientId     string
 	clientSecret string
 	redirectURI  string
@@ -13,8 +17,29 @@ type RefreshingAuthProviderConfig struct {
 	RedirectURI  string
 }
 
-func (p *RefreshingAuthProvider) GetAccessTokenByUserId(userId string) (string, error) {
-	return "", nil
+func (p *RefreshingAuthProvider) AddAccessToken(userId string, accessToken *AccessToken) error {
+	p.accessTokens[accessToken.UserId] = accessToken
+	return nil
+}
+
+func (p *RefreshingAuthProvider) AddAccessTokenFromFile(fileLocation string) error {
+	accessToken, err := CreateAccessTokenFromJSONFile(fileLocation)
+	if err != nil {
+		return err
+	}
+	err = p.AddAccessToken(accessToken.UserId, accessToken)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *RefreshingAuthProvider) GetAccessTokenByUserId(userId string) (*AccessToken, error) {
+	accessToken, ok := p.accessTokens[userId]
+	if !ok {
+		return nil, fmt.Errorf("no access token for user id: %s", userId)
+	}
+	return accessToken, nil
 }
 
 func NewRefreshingAuthProvider(config RefreshingAuthProviderConfig) (*RefreshingAuthProvider, error) {
@@ -30,7 +55,7 @@ func NewRefreshingAuthProvider(config RefreshingAuthProviderConfig) (*Refreshing
 	}
 	// Create provider
 	authProvider := &RefreshingAuthProvider{
-		accessTokens: make(map[string]AccessToken),
+		accessTokens: make(map[string]*AccessToken),
 		clientId:     config.ClientId,
 		clientSecret: config.ClientSecret,
 		redirectURI:  config.RedirectURI,
