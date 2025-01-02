c038f22ea1482e6033412f5fb26fb3b542ec6651
Antony Lee <antony@antonylee.co.uk>
2025-01-02 10:45:36 +0000

    handle config

--- pkg/twitch/refreshing_auth_provider.go
+++ pkg/twitch/refreshing_auth_provider.go
@@ -1,13 +1,39 @@
 package twitch
 
 type RefreshingAuthProvider struct {
+	accessTokens map[string]AccessToken
+	clientId     string
+	clientSecret string
+	redirectURI  string
+}
+
+type RefreshingAuthProviderConfig struct {
+	ClientId     string
+	ClientSecret string
+	RedirectURI  string
 }
 
 func (p *RefreshingAuthProvider) GetAccessTokenByUserId(userId string) (string, error) {
 	return "", nil
 }
 
-func NewRefreshingAuthProvider() (*RefreshingAuthProvider, error) {
-	authProvider := &RefreshingAuthProvider{}
+func NewRefreshingAuthProvider(config RefreshingAuthProviderConfig) (*RefreshingAuthProvider, error) {
+	// Check config
+	if config.ClientId == "" {
+		return nil, ErrBlankClientId
+	}
+	if config.ClientSecret == "" {
+		return nil, ErrBlankClientSecret
+	}
+	if config.RedirectURI == "" {
+		return nil, ErrBlankRedirectURI
+	}
+	// Create provider
+	authProvider := &RefreshingAuthProvider{
+		accessTokens: make(map[string]AccessToken),
+		clientId:     config.ClientId,
+		clientSecret: config.ClientSecret,
+		redirectURI:  config.RedirectURI,
+	}
 	return authProvider, nil
 }
