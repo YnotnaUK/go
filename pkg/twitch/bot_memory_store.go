f8a40ffeb1e2f6eacb5258a412d1522a4f3864d0
Antony Lee <antony@antonylee.co.uk>
2025-01-05 12:20:59 +0000

    initial commit

---
+++ pkg/twitch/bot_memory_store.go
@@ -0,0 +1,28 @@
+package twitch
+
+import (
+	"fmt"
+)
+
+type BotMemoryStore struct {
+	accessTokens map[string]*AccessToken
+}
+
+func (s *BotMemoryStore) GetAccessToken(userId string) (*AccessToken, error) {
+	accessToken, ok := s.accessTokens[userId]
+	if !ok {
+		return nil, fmt.Errorf("no access token for user id: %s", userId)
+	}
+	return accessToken, nil
+}
+func (s *BotMemoryStore) CreateOrUpdateAccessToken(accessToken *AccessToken) (bool, error) {
+	s.accessTokens[accessToken.UserId] = accessToken
+	return true, nil
+}
+
+func NewBotMemoryStore() (*BotMemoryStore, error) {
+	store := &BotMemoryStore{
+		accessTokens: make(map[string]*AccessToken),
+	}
+	return store, nil
+}
