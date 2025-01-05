6dcda78944546e6ee772b5ff18148cd796d7bd19
Antony Lee <antony@antonylee.co.uk>
2025-01-05 12:14:06 +0000

    now expects a store
    * also added validation to NewBot function

--- pkg/twitch/bot.go
+++ pkg/twitch/bot.go
@@ -1,14 +1,22 @@
 package twitch
 
-import "fmt"
+import "errors"
+
+var (
+	ErrNoAuthProvider  error = errors.New("no auth provider provided")
+	ErrNoStoreProvided error = errors.New("no store provided")
+	ErrNoUserId        error = errors.New("no user id provided")
+)
 
 type Bot struct {
 	authProvider AuthProvider
+	store        BotStorer
 	userId       string
 }
 
 type BotConfig struct {
 	AuthProvider AuthProvider
+	Store        BotStorer
 	UserId       string
 }
 
@@ -17,11 +25,22 @@ func (b *Bot) Start() error {
 }
 
 func NewBot(config *BotConfig) (*Bot, error) {
+	// Validate config
+	if config.AuthProvider == nil {
+		return nil, ErrNoAuthProvider
+	}
+	if config.Store == nil {
+		return nil, ErrNoStoreProvided
+	}
+	if config.UserId == "" {
+		return nil, ErrNoUserId
+	}
+	// Create bot
 	bot := &Bot{
 		authProvider: config.AuthProvider,
+		store:        config.Store,
 		userId:       config.UserId,
 	}
-	bot.authProvider.AddAccessTokenFromFile("data/access_token.json")
-	fmt.Println(config.AuthProvider.GetAccessTokenByUserId(bot.userId))
+	// Return bot
 	return bot, nil
 }
